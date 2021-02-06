7ee58eb1385e598d8689e9388a59d47899d21e47
Ertuğrul Emre Cihan <cihanertugrulemre368@gmail.com>
2021-02-07 00:11:54 +0300

    06.02.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,6 +9,23 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            ProductTest();
+
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GettAll())
+            {
+                Console.WriteLine("Ürünün Adı: " + category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
             foreach (var product in productManager.GetAll())
@@ -24,6 +41,11 @@ namespace ConsoleUI
             foreach (var product in productManager.GetByUnitPrice(50, 6000))
             {
                 Console.WriteLine("Ürün Adı: " + product.ProductName);
+            }
+            Console.WriteLine("--------------------------------");
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine("Ürün Adı: " + product.ProductName + "/" + product.CategoryName);
             }
         }
     }
