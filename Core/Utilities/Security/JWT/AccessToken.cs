89d88397005cf0db9d5a9143cf0645429cfbc4b7
Ertuğrul Emre Cihan <cihanertugrulemre368@gmail.com>
2021-02-28 18:17:07 +0300

    27.02.2021

---
+++ Core/Utilities/Security/JWT/AccessToken.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.JWT
+{
+    public class AccessToken
+    {
+        public string Toke { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
